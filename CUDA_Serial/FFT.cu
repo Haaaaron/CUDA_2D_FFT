7b4438c48148adde5da2591429285d2646435bc0
Haaaaron <aaron.haarti@protonmail.com>
2021-10-12 13:06:44 +0300

    transpose kernel missing

--- CUDA_Serial/FFT.cu
+++ CUDA_Serial/FFT.cu
@@ -3,15 +3,17 @@
 #include "main.h"
 #include "fft.h"
 
-__global__ void mykernel() {
+__global__ void transpose() {
     printf("Hello from mykernel\n");
 }
 
 void forward_fft(system_2D<double>& host_system) {
     transform_system_2D device_system(host_system.get_dimensions());
+
     device_system.forward_transform(host_system.get_data());
     device_system.backward_transform(host_system.get_data());
+
     host_system.print();
-    mykernel<<<1,1>>>();
+    
     cudaDeviceSynchronize();
 }
--- CUDA_Serial/fft.h
+++ CUDA_Serial/fft.h
@@ -4,13 +4,13 @@
 
 class transform_system_2D {
     private:
-        cufftHandle to_complex, to_real;
+        cufftHandle to_complex, to_real, complex_to_complex;
         cufftReal *real_buffer;
         cufftComplex *complex_buffer;
         int array_len, batch_size;
 
     public:
-        __host__ transform_system_2D(tuple<int,int> dimensions):
+        transform_system_2D(tuple<int,int> dimensions):
         array_len(get<0>(dimensions)), batch_size(get<1>(dimensions)) {
             // Allocate device memory
             cudaMalloc((void**)&real_buffer, sizeof(cufftReal)*array_len*batch_size);
@@ -25,32 +25,37 @@ class transform_system_2D {
                 CUFFT_R2C, batch_size);
             
             //TODO add excpetion iif cufft plan creation fails
-            //if (res != CUFFT_SUCCESS) {printf("Plan creation fail\n"); return 1}
+            //if (res != CUFFT_SUCCESS) {throw exceptin()}
 
             cufftPlanMany(
                 &to_real, 1, len,
                 NULL, 1, array_len,
                 NULL, 1, array_len,
                 CUFFT_C2R, batch_size);
+            
+            cufftPlanMany(
+                &complex_to_complex, 1, len,
+                NULL, 1, array_len,
+                NULL, 1, array_len,
+                CUFFT_C2C, batch_size);
         }
 
-        __host__ void forward_transform(double* host_array) {
+        void forward_transform(double* host_array) {
 
             cudaMemcpy(real_buffer, host_array, 
                        sizeof(cufftReal)*array_len*batch_size, 
                        cudaMemcpyHostToDevice);
-
             cufftExecR2C(to_complex, real_buffer, complex_buffer);
+            //transpose<<<1,1>>>();
+            cufftExecC2C(complex_to_complex, complex_buffer, complex_buffer, CUFFT_FORWARD);
         }
 
-        __host__ void backward_transform(double* host_array) {
-
+        void backward_transform(double* host_array) {
+            cufftExecC2C(complex_to_complex, complex_buffer, complex_buffer, CUFFT_INVERSE);
+            //transpose<<<1,1>>>();
             cufftExecC2R(to_real, complex_buffer, real_buffer);
-
             cudaMemcpy(host_array, real_buffer,
                        sizeof(cufftReal)*array_len*batch_size,
                        cudaMemcpyDeviceToHost);
-        }
-
-        
+        }     
 };
